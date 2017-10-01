3d6d531a9b88de30b33c8406f44d67811d6a1c10
Tetiana Dushenkivska <myemail@here>
2017-10-02 00:12:21 +0100

    Add test to Bot

--- apps/bot/test/bot_test.exs
+++ apps/bot/test/bot_test.exs
@@ -2,7 +2,8 @@ defmodule BotTest do
   use ExUnit.Case
   doctest Bot
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "bot says hello to the user" do
+    payload = %{"user" => "tanya", "body" => "hello"}
+    assert Bot.answer(payload) == "hello tanya"
   end
 end
