07540a9509ea15cf839cad48dc2990876708f737
Tetiana Dushenkivska <myemail@here>
2017-10-01 22:20:58 +0100

    Add JS

--- apps/chatter_web/assets/js/app.js
+++ apps/chatter_web/assets/js/app.js
@@ -18,12 +18,38 @@ import "phoenix_html"
 // Local files can be imported directly using relative
 // paths "./socket" or full ones "web/static/js/socket".
 
-// import socket from "./socket"
-
 import socket from "./socket"
 
-let channel = socket.channel("room:lobby", {})
-channel.join()
-  .receive("ok", resp => { console.log("Joined successfully", resp) })
-  .receive("error", resp => { console.log("Unable to join", resp) })
+const init = (socket, document) => {
+  let channel = socket.channel("room:lobby", {})
+  channel.join()
+    .receive("ok", resp => { console.log("Joined successfully", resp) })
+    .receive("error", resp => { console.log("Unable to join", resp) })
 
+  let username = document.getElementById("username")
+  let message = document.getElementById("message")
+  let messages = document.getElementById("messages")
+
+  message.addEventListener("keypress", keyPressHandler(username, message, channel))
+
+  channel.on("new_message", renderMessage(messages))
+}
+
+const keyPressHandler = (username, message, channel) => {
+  return (event) => {
+    if (event.charCode == 13) {
+      channel.push("new_message", {
+        user: username.value,
+        body: message.value
+      })
+      message.value = ""
+    }
+  }
+}
+
+const renderMessage = (messages) => ({user, body}) => {
+  const new_message = `<p><b>[${user}]</b>:${body}</p>`
+  messages.insertAdjacentHTML('beforeend', new_message)
+}
+
+init(socket, document)
